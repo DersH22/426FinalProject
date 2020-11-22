1563b9b769176b12caf5b065e15eee7402bc70d1
Neeva <n33vay@live.unc.edu>
2020-11-22 18:42:02 -0500

    homePage formatting

--- homePageRender.js
+++ homePageRender.js
@@ -1,8 +1,8 @@
 
 async function renderHomePage() {
-    let newsFeed = await retrieveNewsFeed()
-    renderNewsFeed(newsFeed.data.results)
-    console.log(newsFeed)
+    let newsFeed = await retrieveNewsFeed();
+    console.log(newsFeed);
+    renderNewsFeed(newsFeed.data.results);
 }
 
 async function retrieveNewsFeed() {
@@ -10,15 +10,21 @@ async function retrieveNewsFeed() {
         method: 'get',
         url: 'https://api.nytimes.com/svc/topstories/v2/politics.json?api-key=SgAae3IpNtJtnG1f2ySeckEkczkNntW0',
       });
-    return result
+    return result;
 }
 
-
-
+async function renderNewsFeed(results) {
+    const $news = $('#newsFeed');
+    for(let i=0; i<4; i++){
+        $news.append(
+            results[i]);
+    }
+};
 
 
 
 
 $(function() {
     renderHomePage();
+
 });
